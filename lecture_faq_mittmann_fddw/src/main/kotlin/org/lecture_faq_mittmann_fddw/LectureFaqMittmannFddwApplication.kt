a139fd6a1ba59e686269171f88c641fa08e095ee
bandit <bandit@mail.com>
2026-06-11 15:00:51 +0200

    initial commit

---
+++ lecture_faq_mittmann_fddw/src/main/kotlin/org/lecture_faq_mittmann_fddw/LectureFaqMittmannFddwApplication.kt
@@ -0,0 +1,11 @@
+package org.lecture_faq_mittmann_fddw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureFaqMittmannFddwApplication
+
+fun main(args: Array<String>) {
+	runApplication<LectureFaqMittmannFddwApplication>(*args)
+}
---
+++ lecture_faq_mittmann_fddw/src/test/kotlin/org/lecture_faq_mittmann_fddw/LectureFaqMittmannFddwApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.lecture_faq_mittmann_fddw
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureFaqMittmannFddwApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
