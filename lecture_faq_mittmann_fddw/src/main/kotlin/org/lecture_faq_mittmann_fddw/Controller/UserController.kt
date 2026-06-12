3fc98d0118173082d8cf953804a9665d68782559
bandit <bandit@mail.com>
2026-06-12 15:16:29 +0200

    Docker-Umgebung hergestellt: Reverseproxy, Volume und Postgres

--- lecture_faq_mittmann_fddw/src/main/kotlin/org/lecture_faq_mittmann_fddw/Controller/UserController.kt
+++ lecture_faq_mittmann_fddw/src/main/kotlin/org/lecture_faq_mittmann_fddw/Controller/UserController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -43,7 +44,7 @@ class UserController(private val service: UserService) {
         service.addUser(firstName, lastName, email)
     }
 
-    @PutMapping("/users/{id}")
+    @PatchMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
     fun editUser(
         @PathVariable id: UUID,
